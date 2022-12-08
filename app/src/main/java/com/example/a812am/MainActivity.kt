6d4ed3534bd2b3b0274917520012bcf50ddc1d05
XydoMX <kurpiewskikacper333@gmail.com>
2022-12-08 19:20:13 +0100

    2

--- app/src/main/java/com/example/a812am/MainActivity.kt
+++ app/src/main/java/com/example/a812am/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.a812am
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
+        findViewById<Button>(R.id.button).text="Działa"
     }
 }
